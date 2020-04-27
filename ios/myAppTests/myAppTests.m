a6084a9ef98bb8b4c79e6bc110d3be71cc4880b0
HaOui <haouiha@hotmail.com>
2020-04-27 16:13:23 +0700

    rename app with react-native-rename-next

--- ios/myAppTests/myAppTests.m
+++ ios/myAppTests/myAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface safetysmartv2Tests : XCTestCase
+@interface myAppTests : XCTestCase
 
 @end
 
-@implementation safetysmartv2Tests
+@implementation myAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
